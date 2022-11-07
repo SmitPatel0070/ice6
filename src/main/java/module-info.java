240e775f6be6a01ac1f460e966aff35dfeaa2b88
SmitPatel0070 <105453013+SmitPatel0070@users.noreply.github.com>
2022-11-06 23:51:38 -0500

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+//Student Name:- Smit Patel
+module ca.durhamcollege.oop3200f2022week8b {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.durhamcollege.oop3200f2022week8b to javafx.fxml;
+    exports ca.durhamcollege.oop3200f2022week8b;
+}
